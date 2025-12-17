7beec6271d4afd3a7db7f73af953d12198059f04
Samuel Moreno <Samuel.Moreno@etu.univ-smb.fr>
2025-12-17 09:04:26 +0100

    Grosse modification de UCJeu, rajout de tout les deplacement possible et action possible dans le dico et creation de la class Personnage.cs pour pouvoir instancier chaque perso different en une ligne

---
+++ Viking/Personnage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using VikingGame;
+
+namespace Viking
+{
+    public class Personnage
+    {
+        public string Nom { get; }
+        public BitmapImage Portrait { get; }
+        public BitmapImage SpriteSheet { get; }
+        public AnimationManager AnimManager { get; }
+
+        public Personnage(
+            string nom,
+            string portraitFile,       // ex: "Viking Portrait.png"
+            string spriteFile,         // ex: "Viking-Sheet.png"
+            Image targetImage,         // ex: imgViking dans UCJeu.xaml
+            int frameWidth,            // largeur d’une frame = 33
+            int frameHeight,           // hauteur d’une frame = 46
+            Dictionary<string, (int row, int frameCount)> animationMap
+        )
+        {
+            Nom = nom;
+
+            // Charger le portrait (optionnel mais utile pour UCMenu plus tard)
+            if (!string.IsNullOrWhiteSpace(portraitFile))
+            {
+                Portrait = new BitmapImage(new Uri($"/img/{portraitFile}", UriKind.Relative));
+            }
+
+            // Charger la spritesheet
+            SpriteSheet = new BitmapImage(new Uri($"/img/{spriteFile}", UriKind.Relative));
+
+            // Créer le gestionnaire d’animations
+            AnimManager = new AnimationManager(targetImage, frameWidth, frameHeight, SpriteSheet, animationMap);
+        }
+
+        public void JouerAnimation(string animationName)
+        {
+            AnimManager.Play(animationName);
+        }
+    }
+}
--- Viking/UCJeu.xaml.cs
+++ Viking/UCJeu.xaml.cs
@@ -1,26 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Media.Media3D;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 
-namespace VikingGame
+namespace Viking
 {
     public partial class UCJeu : UserControl
     {
-        private AnimationManager animManager;
+        private Personnage viking;
 
         public UCJeu()
         {
@@ -30,21 +18,40 @@ namespace VikingGame
 
         private void UCJeu_Loaded(object sender, RoutedEventArgs e)
         {
-            BitmapImage spriteSheet = new BitmapImage(
-                new Uri("pack://application:,,,/VikingGame;component/Viking/viking.png")
-            );
-
-            Dictionary<string, (int row, int frameCount)> animationMap = new Dictionary<string, (int, int)>
+            // Dictionnaire des animations du Viking
+            var vikingAnimationMap = new Dictionary<string, (int, int)>
             {
                 { "Idle", (0, 8) },
                 { "Walk", (1, 8) },
                 { "Run", (2, 8) },
-                { "Attack1", (3, 4) },
-                { "Death", (4, 12) }
+                { "Slide", (3, 7) },
+                { "Crouch", (4, 2) },
+                { "CrouchAttack", (5, 5) },
+                { "Jump", (6, 1) },
+                { "JumpToFall", (7, 3) },
+                { "Fall", (8, 1) },
+                { "JumpAttack", (9, 6) },
+                { "IdleBlock", (10, 8) },
+                { "Block", (11, 5) },
+                { "Attack1", (12, 4) },
+                { "Attack2", (13, 4) },
+                { "Attack3", (14, 4) },
+                { "Spell", (15, 12) },
+                { "SpellSlam", (16, 11) },
+                { "LadderClimb", (17, 8) },
+                { "WallHang", (18, 6) },
+                { "WallClimb", (19, 5) },
+                { "TransformationOut", (20, 8) },
+                { "Dash", (21, 5) },
+                { "Hit", (22, 4) },
+                { "Death", (23, 12) }
             };
 
-            animManager = new AnimationManager(imgViking, 33, 46, spriteSheet, animationMap);
+            // Création du personnage Viking
+            viking = new Personnage("Viking", "Viking Portrait.png", "Viking-Sheet.png", imgViking, 33, 46, vikingAnimationMap);
 
+
+            // Brancher les touches clavier
             Window parentWindow = Window.GetWindow(this);
             if (parentWindow != null)
             {
@@ -56,25 +63,25 @@ namespace VikingGame
         {
             if (e.Key == Key.Right)
             {
-                animManager.Play("Run");
+                viking.JouerAnimation("Run");
                 Canvas.SetLeft(imgViking, Canvas.GetLeft(imgViking) + 5);
             }
             else if (e.Key == Key.Left)
             {
-                animManager.Play("Walk");
+                viking.JouerAnimation("Walk");
                 Canvas.SetLeft(imgViking, Canvas.GetLeft(imgViking) - 5);
             }
             else if (e.Key == Key.Space)
             {
-                animManager.Play("Attack1");
+                viking.JouerAnimation("Attack1");
             }
             else if (e.Key == Key.D)
             {
-                animManager.Play("Death");
+                viking.JouerAnimation("Death");
             }
             else if (e.Key == Key.I)
             {
-                animManager.Play("Idle");
+                viking.JouerAnimation("Idle");
             }
         }
     }
